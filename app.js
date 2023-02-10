742f9d23cba9626ae0adc7e230f58c0cade0b7a3
Gabriel Vallejo <gabrielvallejo2000@gmail.com>
2023-02-10 12:00:43 -0600

    test

--- app.js
+++ app.js
@@ -1,28 +1,32 @@
-fetch('api/proyectos.php').then(response => response.json()).then(datos => {
-  let html = "";
-  datos.forEach(consulta => {
-    html += `
-    <tr>
-      <td>
-        ${consulta.nombre}
-      </td>
-      <td>
-      ${consulta.fecha_inicio}
-      </td>
-      <td>
-      ${consulta.fecha_final}
-      </td>
-      <td>
-      <span class="badge bg-${consulta.type}">${consulta.estado}</span>
-      </td>
-      <td>
-      ${consulta.asignado}
-      </td>
-      <td>
-        <a href="#" class="text-danger"> Eliminar </a>
-      </td>
-    </tr>
-    `;
+function actualizar(){
+  fetch('api/proyectos.php').then(response => response.json()).then(datos => {
+    let html = "";
+    datos.forEach(consulta => {
+      html += `
+      <tr>
+        <td>
+          ${consulta.nombre}
+        </td>
+        <td>
+        ${consulta.fecha_inicio}
+        </td>
+        <td>
+        ${consulta.fecha_final}
+        </td>
+        <td>
+        <span class="badge bg-${consulta.type}">${consulta.estado}</span>
+        </td>
+        <td>
+        ${consulta.asignado}
+        </td>
+        <td>
+          <a href="#" class="text-danger"> Eliminar </a>
+        </td>
+      </tr>
+      `;
+    });
+    document.getElementById('tabla_proyectos').innerHTML = html;
   });
-  document.getElementById('tabla_proyectos').innerHTML = html;
-});
+}
+actualizar();
+setInterval(actualizar,1000);
