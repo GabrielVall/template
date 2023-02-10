41aae904e8bdad80ad4d7c0dc5e0ef5b05dfc859
Gabriel Vallejo <gabrielvallejo2000@gmail.com>
2023-02-10 11:10:19 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+fetch('api/proyectos.php').then(response => response.json()).then(datos => {
+  let html = "";
+  datos.forEach(consulta => {
+    html += `
+    <tr>
+      <td>
+        ${consulta.nombre}
+      </td>
+      <td>
+      ${consulta.fecha_inicio}
+      </td>
+      <td>
+      ${consulta.fecha_final}
+      </td>
+      <td>
+      <span class="badge bg-${consulta.type}">${consulta.estado}</span>
+      </td>
+      <td>
+      ${consulta.asignado}
+      </td>
+      <td>
+        <a href="#" class="text-danger"> Eliminar </a>
+      </td>
+    </tr>
+    `;
+  });
+  document.getElementById('tabla_proyectos').innerHTML = html;
+});
